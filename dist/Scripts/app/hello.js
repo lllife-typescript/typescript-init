93e9ef094c582ba7c4eb66e89c67b09491361dd9
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-09-27 00:39:34 +0700

    follow egghead.io

--- dist/Scripts/app/hello.js
+++ dist/Scripts/app/hello.js
@@ -1,29 +1,20 @@
 var app;
 (function (app) {
-    function searchWikipedia(term) {
-        return $.ajax({
-            url: "http://en.wikipedia.org/w/api.php",
-            dataType: "jsonp",
-            data: {
-                action: "opensearch",
-                format: "json",
-                search: term
-            }
-        }).promise();
-    }
-    function test() {
-        var input = $("#input");
-        var result = $("#result");
-        var keyups = Rx.Observable.fromEvent(input, "keyup")
-            .map(function (e) { return e.target.value; })
-            .filter(function (text) { return text.length > 2; });
-        var throttled = keyups.throttle(500);
-        var distint = throttled.distinctUntilChanged();
-        var suggestion = distint.flatMapLatest(searchWikipedia);
-        suggestion.subscribe(function (data) {
-            var res = data[1];
-            result.empty();
+    function hello() {
+        var source = Rx.Observable.create(function (observer) {
+            setTimeout(function () {
+                console.log("timeout hit");
+                observer.onNext(42);
+                observer.onCompleted();
+            }, 1000);
+            console.log("started");
         });
+        var go = function (x) { console.log(x); };
+        var error = function (err) { console.error(err); };
+        var done = function () { console.log("done."); };
+        var sub = source.subscribe(go, error, done);
+        setTimeout(function () {
+        }, 500);
     }
-    test();
+    hello();
 })(app || (app = {}));
--- src/hello.ts
+++ src/hello.ts
@@ -1,31 +1,27 @@
 
 module app {
-    function searchWikipedia(term) {
-        return $.ajax({
-            url: "http://en.wikipedia.org/w/api.php",
-            dataType: "jsonp",
-            data: {
-                action: "opensearch",
-                format: "json",
-                search: term
-            }
-        }).promise();
-    }
 
-    function test() {
-        var input = $("#input")
-        var result = $("#result")
-        var keyups = Rx.Observable.fromEvent(input, "keyup")
-            .map(e => e.target.value)
-            .filter( text => text.length > 2);
-        var throttled = keyups.throttle(500);
-        var distint = throttled.distinctUntilChanged();
-        var suggestion = distint.flatMapLatest(searchWikipedia);
+    function hello() {
+        var source = Rx.Observable.create( observer => {
+            setTimeout( () => {
+                console.log("timeout hit");
+                observer.onNext(42);
+                observer.onCompleted();
+            }, 1000);
 
-        suggestion.subscribe( data => {
-            var res = data[1]
-            result.empty();
+            console.log("started");
         });
+
+        var go = (x) => { console.log(x); }
+        var error = (err) => { console.error(err); }
+        var done = () => { console.log("done.")}
+
+        var sub = source.subscribe(go, error, done);
+
+        setTimeout(() => {
+            //sub.dispose();
+        }, 500);
     }
-    test();
+
+    hello();
 }
