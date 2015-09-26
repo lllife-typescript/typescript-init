e770033b4679e63346ab1edc01de1964a18fb092
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-09-27 02:22:30 +0700

    test fromEvent method

--- dist/Scripts/app/hello.js
+++ dist/Scripts/app/hello.js
@@ -3,18 +3,22 @@ var app;
     function hello() {
         var source = Rx.Observable.create(function (observer) {
             setTimeout(function () {
-                console.log("timeout hit");
                 observer.onNext(42);
                 observer.onCompleted();
             }, 1000);
-            console.log("started");
         });
         var go = function (x) { console.log(x); };
         var error = function (err) { console.error(err); };
         var done = function () { console.log("done."); };
         var sub = source.subscribe(go, error, done);
-        setTimeout(function () {
-        }, 500);
+    }
+    function click() {
+        var button = document.getElementById("button");
+        var source = Rx.Observable.fromEvent(button, "click");
+        source.forEach(function () {
+            console.log("click");
+        });
     }
     hello();
+    click();
 })(app || (app = {}));
--- src/hello.ts
+++ src/hello.ts
@@ -4,24 +4,25 @@ module app {
     function hello() {
         var source = Rx.Observable.create( observer => {
             setTimeout( () => {
-                console.log("timeout hit");
                 observer.onNext(42);
                 observer.onCompleted();
             }, 1000);
-
-            console.log("started");
         });
 
         var go = (x) => { console.log(x); }
         var error = (err) => { console.error(err); }
         var done = () => { console.log("done.")}
-
         var sub = source.subscribe(go, error, done);
+    }
 
-        setTimeout(() => {
-            //sub.dispose();
-        }, 500);
+    function click() {
+        var button = document.getElementById("button");
+        var source = Rx.Observable.fromEvent(button, "click");
+        source.forEach(() => {
+            console.log("click");
+        });
     }
 
     hello();
+    click();
 }
